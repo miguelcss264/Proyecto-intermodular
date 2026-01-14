22f322cc1bff9e93e68c9853a4a0eb1fdaff9894
Marcos <mariro@alumnatflorida.es>
2026-01-14 10:27:40 +0100

    Añadimos etiquetas al html

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=!, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Alta Frecuencia Podcast - Inicio</title>
+</head>
+<body>
+    <header></header>
+    <nav></nav>
+    <section></section>
+    <article></article>
+    <aside></aside>
+    <footer></footer>
+</body>
+</html>
